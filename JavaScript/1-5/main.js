23c1262b8e29bc12449d218ce134f2c96dafc059
m-kusumi <t22dunk@gmail.com>
2023-02-09 20:59:40 +0900

    20230209_課題提出_関数
    1章_JavaScript初級_関数

---
+++ JavaScript/1-5/main.js
@@ -0,0 +1,7 @@
+function createjuice(fruits){
+    let juice = fruits + `ジュース`
+    return juice;
+}
+let orangejuice = createjuice(`みかん`);
+console.log(orangejuice + `を受け取りました。ジュースにして返します`);
+console.log(orangejuice + `が届きました`);
