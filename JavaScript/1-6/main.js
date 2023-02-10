206210d4fe507955df883eff2b76dd07b70eb985
m-kusumi <t22dunk@gmail.com>
2023-02-10 20:42:51 +0900

    20230210_課題提出_配列・オブジェクト
    1章_JavaScript初級_配列・オブジェクト

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,10 @@
+let scores = [10, 15, 20, 25];
+console.log(scores[0] + `は偶数です`);
+console.log(scores[2] + `は偶数です`);
+
+let car = {
+    gass: 20.5,
+    num : 1234,
+};
+console.log(`ガソリンは` + car.gass + `です`);
+console.log(`ナンバーは` + car.num + `です`);
