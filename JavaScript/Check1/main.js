2a7866a17993d18377b76708ace69878a3ece9c5
m-kusumi <t22dunk@gmail.com>
2023-02-15 22:41:53 +0900

    20230215_課題提出_1章チェックテスト
    課題提出_1章チェックテスト

---
+++ JavaScript/Check1/main.js
@@ -0,0 +1,17 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+class Car{
+    //ここに答えを実装してください。↓↓↓
+    constructor(gas,numbers){
+        this.gas = gas;
+        this.num = numbers;
+    }
+    //「ガソリンは〇〇です。ナンバーは△△です」と出力される「getNumGas」という関数
+    getNumGas(){
+        console.log(`ガソリンは${this.gas}です。ナンバーは${this.num}です。`);
+    }
+}
+    function isEven(num) {
+        return num % 2 === 0;
+        console.log(num + `は偶数です`);
+    }
+    num.getNumGas();
